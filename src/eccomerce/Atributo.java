ca77a9c7536e76bc9fe21815053a64468c8d22de
Martin Tchurukian <mat34218@gmail.com>
2026-06-05 14:44:16 -0300

    algunos mensajes de la clase Item

---
+++ src/eccomerce/Atributo.java
@@ -0,0 +1,5 @@
+package eccomerce;
+
+public class Atributo {
+
+}
--- src/eccomerce/Item.java
+++ src/eccomerce/Item.java
@@ -1,8 +1,32 @@
 package eccomerce;
 
-public class Item {
+import java.util.ArrayList;
+
+abstract public class Item {
 	
-	private String nombre;
-	private String sku;
+	private String 	nombre;
+	private String 	sku;
+	private String 	marca;
+	private String	categoria;
+	private double 	precioBase;
+	private double 	descuentoPromocional;
+	private int 	stock;
+	private ArrayList<Atributo> atributos = new ArrayList<>();
+
+
+
+abstract protected void 	incrementarStock();
+abstract protected void 	incrementarStockEn(int cantidad);
+abstract protected void 	decrementarStock();
+abstract protected double 	getPrecioBase();
+abstract protected double 	getPrecioFinal();
+abstract protected boolean 	existeAtributoOpcional(String nombreAtributo);
+abstract protected String 	getAtributoOpcional(String nombreAtributo);
+abstract protected boolean 	esItemValido();
+abstract protected String	getNombre();
+abstract protected String	getCategoria();
+abstract protected String	getSKU();
+abstract protected int		getStock();
+abstract protected boolean	hayStock();
 
 }
