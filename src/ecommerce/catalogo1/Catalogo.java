788522dcfcd9744d21ceda15741b3b85b67818ed
Martin Tchurukian <mat34218@gmail.com>
2026-07-01 18:24:25 -0300

    catalogo reporta ventas

--- src/ecommerce/catalogo1/Catalogo.java
+++ src/ecommerce/catalogo1/Catalogo.java
@@ -3,6 +3,7 @@ package ecommerce.catalogo1;
 import java.util.ArrayList;
 
 import ecommerce.busqueda6.CriterioDeBusqueda;
+import ecommerce.reportes.ItemVisitor;
 
 public class Catalogo {
 	
@@ -39,4 +40,14 @@ public class Catalogo {
         
         return resultados;
     }
+	
+	public String reportar(ItemVisitor unVisitor) {
+	    
+	    for (Item item : this.itemsDelCatalogo) {
+	        item.accept(unVisitor); // el visitor concreto elegido visita cada item
+	    }
+	    
+	    
+	    return unVisitor.getResultado(); //obtenemos el resultado
+	}
 }
